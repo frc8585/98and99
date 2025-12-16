0798bebcd63884609313b9e37cc2bd8145e219a3
opzen <eden20090817@gmail.com>
2025-12-16 17:44:40 +0800

    氣壓

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
+  private Compressor m_Compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+  private Solenoid m_Solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+  private Joystick m_Joystick = new Joystick(0);
 
   public Robot() {}
 
@@ -19,5 +26,8 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if(m_Joystick.getRawButton(0))
+      m_Solenoid.set(true);
+  }
 }
